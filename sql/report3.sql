ebec14fe00a944dc02b04d620fe891fa363729d7
Gennady Ladyrda <gennady.ladyrda@yandex.ru>
2018-03-14 13:44:32 +0300

    Report 3 changed

--- sql/report3.sql
+++ sql/report3.sql
@@ -2,7 +2,7 @@ set term off
 set linesize 999
 set wrap on
 spool ./reports/3.rep
-  select author_name
+  select author_name, rnk as popularity
     from (select a.author_name, a.athr_id, count(*), row_number() over (order by count(*) desc) as rnk
             from (select b.athr_id from facts f, books b where b.book_id = f.book_id) fb, authors a
           where a.athr_id = fb.athr_id
